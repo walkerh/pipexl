a71771169efd9a1391c74f0aed546dcb50517555
Walker Hale IV <walker.hale.iv@gmail.com>
2018-11-09 14:15:35 -0600

    Initial commit

---
+++ pipexl/__init__.py
@@ -0,0 +1,108 @@
+"""Code for constructing data pipelines that involve tables inside
+workbooks."""
+
+
+from dataclasses import make_dataclass, fields, astuple
+from pathlib import Path
+
+
+from openpyxl import load_workbook
+
+
+class WorkbookModel:
+    """Encapsulates a list of Table subclasses with a naming
+    pattern for the workbook. Subclasses should nest subclasses of `Table` and
+    define the classes attribute `name_pattern` as a glob."""
+    name_pattern = None  # subclasses should override
+
+    def __init__(self, config=None):
+        """The `config` parameter should contain a key matching
+        `self.__class__.__name__` with the directory path to search for the
+        workbook."""
+        config = config or {}
+        directory_path = Path(config.get(self.__class__.__name__, '.'))
+        hits = sorted(directory_path.glob(self.name_pattern))
+        assert hits
+        hit = hits[-1]  # taking the highest as most recent
+        table_classes = [v for v in self.__class__.__dict__.values()
+                         if isinstance(v, type) and issubclass(v, Table)]
+        self.workbook_path = str(hit)
+        self.workbook = load_workbook(self.workbook_path,
+                                      read_only=True,
+                                      data_only=True)
+        self.tables = []
+        for table_class in table_classes:
+            table = table_class(self.workbook)
+            self.tables.append(table)
+            setattr(self, table.name, table)
+
+
+class Table:
+    """A table in a worksheet. Subclasses should override class attributes
+    `name`, `worksheet_name`, `table_marker`, and `key_column_names` (list)."""
+    def __init__(self, workbook):
+        self.worksheet = workbook[self.worksheet_name]
+        row_iter = self.worksheet.rows
+        # Find cell with table marker.
+        for row in row_iter:
+            # Find first non-blank cell in this row.
+            for cell in row:
+                if cell.value:
+                    break
+            if cell.value == self.table_marker:
+                break
+        assert cell.value == self.table_marker, cell
+        self.start_row = cell.row + 1  # Row of first record
+        # Load header.
+        self.start_col = cell.column - 1
+        raw_header = [n.value for n in next(row_iter)[self.start_col:]]
+        limit = None
+        for i, name in enumerate(raw_header):
+            if name == 'separator' or not name:
+                limit = i
+                break
+        if limit is None:
+            limit = len(raw_header)
+        self.header = [normalize_field_name(n) for n in raw_header[:limit]]
+        # Define record class.
+        self.stop_col = self.start_col + limit
+        self.record_class = make_record_class(self.name, self.header)
+        # # Load data.
+        self.data = [
+            self.record_class(*(c.value for c in row[self.start_col:self.stop_col]))
+            for row in row_iter
+        ]
+
+
+def normalize_field_name(field_name):
+    """lowercase with underscores, etc"""
+    result = field_name or None
+    if result:
+        if result.endswith('?'):
+            result = result[:-1]
+            if not result.startswith('is_'):
+                result = 'is_' + result
+        result = result.strip().lower().replace(' ', '_').replace(
+            '-', '_').replace('/', '_per_').replace('?', '_').replace(
+            '%', 'pct').replace('.', '')
+    return result
+
+
+def make_record_class(cls_name, fields):
+    """Return a class for holding table rows as records. The data can be
+    accessed using either attribute or dictionary syntax."""
+    return make_dataclass(cls_name,
+                          fields,
+                          bases=(RecordAttributeMixin,),
+                          frozen=True)
+
+
+class RecordAttributeMixin:
+    astuple = property(astuple)
+
+    @property
+    def fields(self):
+        return tuple(f.name for f in fields(self))
+
+    def __getitem__(self, name):
+        return self.__dict__[name]
