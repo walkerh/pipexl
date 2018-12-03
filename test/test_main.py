ac3d7ef1fd533f1230446fc70a27b8b1d660dc19
Walker Hale IV <walker.hale.iv@gmail.com>
2018-12-03 16:49:35 -0600

    Tests for WorkbookModel and Table

---
+++ test/test_main.py
@@ -0,0 +1,74 @@
+"""Tests for top level pipexl package."""
+
+from pipexl import Table, WorkbookModel
+
+
+class WorkbookforTesting(WorkbookModel):
+    """Model for book_a.xlsx"""
+    name_pattern = 'book_?.xlsx'
+    class TableForTesting(Table):
+        """Model for the one sheet in that workbook."""
+        worksheet_name = 'sheet_a'
+        name = 'test_table'
+        table_marker = 'table_marker'
+        key_fields = (
+            'key_a',
+            'key_b',
+        )
+        normalize_fields = (
+            'value_b',
+        )
+        filters = dict(key_b='Total')
+
+
+CONFIG = dict(WorkbookforTesting='test/resources')
+WORKBOOK = WorkbookforTesting(CONFIG)
+TEST_TABLE = WORKBOOK.test_table
+EXPECTED_KEY_A = ['agree million soon',
+                  'because week were',
+                  'help slowly crowd',
+                  'sound rolled table',
+                  'taste strange written',
+                  'unit food held']
+
+
+def test_workbook_attributes():
+    assert sorted(vars(WORKBOOK)) == [
+        'test_table', 'workbook_path'
+    ]
+
+
+def test_table_class_name():
+    assert TEST_TABLE.record_class.__name__ == 'test_table'
+
+
+def test_table_fields():
+    assert TEST_TABLE.key_fields == ('key_a', 'key_b')
+    assert TEST_TABLE.non_key_fields == (
+        'value_a', 'value_b', 'value_c', 'jan_19', 'feb_19'
+    )
+    assert set(TEST_TABLE.fields) == set(TEST_TABLE.key_fields +
+                                         TEST_TABLE.non_key_fields)
+
+
+def test_record_contents():
+    r = TEST_TABLE[0]
+    assert r.fields == TEST_TABLE.fields
+    assert vars(r) == dict(feb_19=66.47,
+                           jan_19=None,
+                           key_a='unit food held',
+                           key_b='Africa neighbor French',
+                           value_a=79.82,
+                           value_b='so_1_qp',
+                           value_c=20.8)
+
+
+def test_total_records_filtered_out():
+    records_with_total = [r for r in TEST_TABLE if 'Total' == r.key_b]
+    for r in records_with_total:
+        print(r)
+    assert not records_with_total
+
+
+def test_recipes():
+    assert set(r.key_a for r in TEST_TABLE) == set(EXPECTED_KEY_A)
